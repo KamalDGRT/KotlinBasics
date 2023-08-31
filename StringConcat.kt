005e95b5cb738441c0d155697ccb2e455791a954
KamalDGRT <kamaldgrt@gmail.com>
2023-08-31 15:51:12 +0530

    add string concat example

---
+++ StringConcat.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+
+    val start = "Talk is cheap. "
+    val middle = "Show me the code. "
+    val end = "- Linus Torvalds"
+
+    val result = start + middle + end
+    println(result)
+}
